460a1fab59c417bee534bd44cec1990148ecb1f7
Evilrabbit520 <evilrabbit520@gmail.com>
2021-12-29 22:04:24 +0800

    Implement md5 and simple unit testing

---
+++ Tests/MD5UnitTests/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file UnitTest/main.cpp
+ * @author Evilrabbit (evilrabbit520@gmail.com)
+ * @brief Unit test.
+ * @version 1.0.1
+ * @date 2021-12-29
+ * 
+ * @copyright Copyright (c) 2021 Evilrabbit. All rights reserved.
+ * 
+ */
+#include <iostream>
+#include <cstring>
+#include <thread>
+
+#include "include/main.h"
+#include "include/md5.h"
+
+using namespace std;
+
+
+int main()
+{
+    hello();
+
+	return 0;
+}
---
+++ build/ETISrc/EncryptionAlgorithm/CMakeFiles/OpenETI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../lib/libOpenETI.dylib"
+  "../../lib/libOpenETI.pdb"
+  "CMakeFiles/OpenETI.dir/main.cpp.o"
+  "CMakeFiles/OpenETI.dir/md5.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenETI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
